52e36968fcae42aedb0dcd54a1aef2fa2608cb53
robinchenyu <robin.chenyu@gmail.com>
2014-08-27 12:31:05 +0800

    delete all files

--- init-jedi.el
+++
@@ -1,8 +0,0 @@
-(require-package 'jedi)
-(add-hook 'python-mode-hook 'jedi:setup)
-(add-hook 'python-mode-hook 'jedi:ac-setup)
-(setq jedi:setup-keys t)
-(setq jedi:complete-on-dot t )
-(setq jedi:server-args
-      '("--virtual-env" "/home/robin/Env/mezproj"))
-(provide 'init-jedi)
