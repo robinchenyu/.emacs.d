8fc9bcdc53998a108926be7ced7f533c7d6480f4
robinchenyu <robin.chenyu@gmail.com>
2014-11-04 09:45:13 +0800

    add slime compat for sbcl

--- init-cygwin-compat.el
+++ init-cygwin-compat.el
@@ -39,4 +39,19 @@
         (inhibit-file-name-operation operation))
     (apply operation args)))
 
+(defun slime-init-command (port-filename _coding-system)
+  "Return a string to initialize Lisp."
+  (let ((loader (if (file-name-absolute-p slime-backend)
+                    slime-backend
+                  (concat slime-path slime-backend))))
+    ;; Return a single form to avoid problems with buffered input.
+    (format "%S\n\n"
+            `(progn
+               (load ,(cygwin-convert-file-name-to-windows (expand-file-name loader))
+                     :verbose t)
+               (funcall (read-from-string "swank-loader:init"))
+               (funcall (read-from-string "swank:start-server")
+                        ,(cygwin-convert-file-name-to-windows port-filename))))))
+
+
 (provide 'init-cygwin-compat)
